a7e01718c501d7f1bc16aff8d91f36e5759e1672
Sarah Pethani <spethani19@roslynschools.org>
2016-10-05 20:01:40 +0000

    Add key binding to that waws easy button

--- that_was_easy/main.js
+++ that_was_easy/main.js
@@ -6,7 +6,8 @@ $("#easy").on("click", sayThatWasEasy);
 $(document).keypress(delegateKeypress);
 function delegateKeypress(event) {
     if(event.charCode==32){
-    var thatWasEasy=new Audio("that_was_easy");
+    var thatWasEasy=new Audio("that_was_easy.mp3");
     thatWasEasy.play();
+    $("#easy").trigger("click");
     }
 }
