4d59210ea11161f0b8468c4a5dd6b65125267926
Sarah Pethani <spethani19@roslynschools.org>
2016-11-02 19:30:56 +0000

    Add colors to geometric_pattern.

--- geoemtric_pattern/main.js
+++ geoemtric_pattern/main.js
@@ -1,5 +1,8 @@
 var NUM_CIRCLES=12;
 var circleDiameter;
+var rVal;
+var gVal;
+var bVal;
 
 function setup() {
     createCanvas(480,600);
@@ -7,9 +10,12 @@ function setup() {
     circleRadius=circleDiameter/2;
 }
 function draw() {
+    rVal=255;
+    gVal=0;
+    bVal=0;
     var isShifted=false;
-    var y=0;
-    while(y<=height) {
+    var y=height;
+    while(y>=0) {
     var x;
     if(isShifted) {
         x=circleRadius;
@@ -18,10 +24,17 @@ function draw() {
         x=0;
     }
     while(x<=width ) {
+        fill(color(rVal, gVal, bVal));
+        stroke(color(rVal, gVal, bVal));
         ellipse(x, y, circleDiameter, circleDiameter);
         x=x+circleDiameter;
     }
-    y=y+circleRadius;
+    y=y-circleRadius;
     isShifted = !isShifted;
+    
+    rVal=rVal-2;
+    gVal=gVal+7;
+    bVal=bVal+3;
     }
 }
+
